1142d10e5656f8a9c5ea4fb844be669642c6cebb
Vikash <vikash.v@monraft.com>
2023-06-20 20:14:53 +0530

    added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,21 +8,34 @@ export default function Document() {
         <link href='https://fonts.googleapis.com/css2?family=Epilogue&display=swap' rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GTM-PF9FGD4"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'GTM-PF9FGD4');
-        `}
-      </Script>
+    id="googleTagManager"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var
+      f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js
+      id=GTM-PF9FGD4'+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer', 'GTM-PF9FGD4');
+      `,
+    }}
+  />
       </Head>
       <body>
-        
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=GTM-PF9FGD4`}
+          height="0"
+          width="0"
+          title="googleTagManagerNoScript"
+          style={{
+            display: 'none',
+            visibility: 'hidden',
+          }}
+        />
+      </noscript>
         <Main />
         <NextScript />
       </body>

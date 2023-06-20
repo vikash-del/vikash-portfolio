ed9f838f6653af3126d3931800a5175f569ae959
Vikash <vikash.v@monraft.com>
2023-06-20 19:55:49 +0530

    added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -6,22 +7,19 @@ export default function Document() {
       <Head>
         <link href='https://fonts.googleapis.com/css2?family=Epilogue&display=swap' rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-        <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=GTM-PF9FGD4`}
-        />
-        <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'GTM-PF9FGD4', {
-                page_path: window.location.pathname,
-              });
-            `,
-            }}
-          />
+        <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-PF9FGD4"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GTM-PF9FGD4');
+        `}
+      </Script>
       </Head>
       <body>
         

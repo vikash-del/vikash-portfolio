b97ee07dd3b1703db5e4d7bfe12b1c5a569debd9
Vikash <vikash.v@monraft.com>
2023-06-20 20:20:57 +0530

    added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <Script
+    id="googleTagManager"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var
+      f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js
+      id=GTM-PF9FGD4'+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer', 'GTM-PF9FGD4');
+      `,
+    }}
+  />
         <link href='https://fonts.googleapis.com/css2?family=Epilogue&display=swap' rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>

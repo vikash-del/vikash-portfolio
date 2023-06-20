0aa24af1ddeb2d8084d1f13593b632b3634914c5
Vikash <vikash.v@monraft.com>
2023-06-20 19:17:27 +0530

    added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,25 @@ export default function Document() {
       <Head>
         <link href='https://fonts.googleapis.com/css2?family=Epilogue&display=swap' rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=GTM-PF9FGD4`}
+        />
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'GTM-PF9FGD4', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+          />
       </Head>
       <body>
+        
         <Main />
         <NextScript />
       </body>

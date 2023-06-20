b96f40dad4a2bb80bece231daf42da70f4eaec71
Vikash <vikash.v@monraft.com>
2023-06-20 20:17:49 +0530

    added gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 import Layout from '../components/Layout/layout'
 
@@ -6,6 +7,21 @@ import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Layout>
+    <Script
+    id="googleTagManager"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var
+      f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js
+      id=GTM-PF9FGD4'+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer', 'GTM-PF9FGD4');
+      `,
+    }}
+  />
     <Component {...pageProps} />
   </Layout>
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+
 
 export default function Document() {
   return (
@@ -7,21 +7,6 @@ export default function Document() {
       <Head>
         <link href='https://fonts.googleapis.com/css2?family=Epilogue&display=swap' rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-        <Script
-    id="googleTagManager"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var
-      f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js
-      id=GTM-PF9FGD4'+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer', 'GTM-PF9FGD4');
-      `,
-    }}
-  />
       </Head>
       <body>
       <noscript>
